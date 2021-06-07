ef196d7cc534d7f7ddca45e34ebe4b9814f2b1f9
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-07 13:20:51 -0700

    add array loop, not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
   $("form#test-form").submit(function(event) {
     event.preventDefault();
-    array = [];
+    console.log("working!")
+    let array = [];
     
     array.forEach(function(element) {
-      let q1 = $("q1").val();
-      array.push(q1);
-      console.log(array);
+      console.log("forEach loop");
     });
 
 
