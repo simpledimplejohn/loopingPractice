bbf9f448430c241cafc193ac1469e568e7933073
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-07 13:37:23 -0700

    add forms to array in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
   $("form#test-form").submit(function(event) {
     event.preventDefault();
-    console.log("working!")
+    let question1 = $(q1).val();
+    let question2 = $(q2).val();
+    let question3 = $(q3).val();
+    let question4 = $(q3).val();
+    console.log("element: " + question1);
+    console.log("element: " + question2);
+    console.log("element: " + question3);
+    console.log("element: " + question4);
     let array = [];
-    
-    array.forEach(function(element) {
-      console.log("forEach loop");
-    });
+    array.push(question1);
+    array.push(question2);
+    array.push(question3);
+    array.push(question4);
+    console.log("array: " + array);
 
 
 
